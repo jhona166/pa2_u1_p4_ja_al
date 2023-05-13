24b5856150f5811c959e4b9e8859f361a5ee8509
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-05-12 19:40:12 -0500

    Cambios despues del almuerzo

---
+++ src/main/java/com/example/demo/Pa2U1P4JaAlApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P4JaAlApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P4JaAlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto");
+		/*
+		 * Spring framework: Framework que permite la construccion/desarrollo de 
+		 * aplicaciones empresariales basadas en Spring
+		 */
+	}
+
+}
