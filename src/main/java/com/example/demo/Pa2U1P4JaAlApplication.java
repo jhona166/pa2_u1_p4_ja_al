cbc0d7e8c96bac36d741d4945d3e2158f82c2deb
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-05-15 19:38:08 -0500

    Taller4

--- src/main/java/com/example/demo/Pa2U1P4JaAlApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4JaAlApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,13 @@ public class Pa2U1P4JaAlApplication implements CommandLineRunner{
 		 * Spring framework: Framework que permite la construccion/desarrollo de 
 		 * aplicaciones empresariales basadas en Spring
 		 */
+		
+		Profesor profe =new Profesor();
+		profe.setApellido("Altamirano");
+		profe.setCedula("1727501510");
+		profe.setFechaNacimiento(LocalDateTime.now());
+		profe.setNombre("Jhonatan");
+		System.out.println(profe);
 	}
 
 }
